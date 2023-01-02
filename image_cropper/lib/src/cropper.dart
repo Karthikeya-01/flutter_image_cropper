570ecf0eadd76d2aee09bad3ecc71fe98bbc0e25
Karthikeya-01 <76080810+Karthikeya-01@users.noreply.github.com>
2023-01-02 17:35:11 +0530

    Update Cropper.dart

--- image_cropper/lib/src/cropper.dart
+++ image_cropper/lib/src/cropper.dart
@@ -74,7 +74,8 @@ class ImageCropper {
       CropAspectRatioPreset.square,
       CropAspectRatioPreset.ratio3x2,
       CropAspectRatioPreset.ratio4x3,
-      CropAspectRatioPreset.ratio16x9
+      CropAspectRatioPreset.ratio16x9,
+      CropAspectRatioPreset.ratio11x8
     ],
     CropStyle cropStyle = CropStyle.rectangle,
     ImageCompressFormat compressFormat = ImageCompressFormat.jpg,
